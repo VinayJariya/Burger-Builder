f32a15bc5bde1cae24f934071ede9903e44a9b35
Vinay Jariya <vinayjariya@gmail.com>
2019-09-07 19:59:58 +0530

    Added Toolbar and NavigationBar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 
 import Aux from '../../hoc/Aux';
-import Burger from '../Burger/Burger';
-import BuilControls from '../Burger/BuildControls/BuildControls'
-import Modal from '../UI/Modal/Modal';
-import OrderSummary from '../Burger/OrderSummary/OrderSummary';
+import Burger from '../../components/Burger/Burger';
+import BuilControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
