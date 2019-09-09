bb0db94c0209066a4809bb3e6210ddde8faf4053
Vinay Jariya <vinayjariya@gmail.com>
2019-09-10 00:43:34 +0530

    Implemented Routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,29 +103,17 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Vinay',
-                address: {
-                    city: 'Bengaluru',
-                    state: 'Karnataka',
-                    country: 'India',
-                    zipCode: '12345'
-                },
-                email: 'test@test.com',
-            },
-            deliveryMethod: 'fastes',
+        const queryParams = [];
+        for ( let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-        .then(response => {
-            this.setState({loading: false, purchasing: false});
-        }).catch(error => {
-            this.setState({loading: false, purchasing: false});
-            console.log(error)
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString,
         });
+
     }
 
     render(){
