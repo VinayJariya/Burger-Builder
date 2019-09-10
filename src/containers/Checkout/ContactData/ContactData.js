f2b945d9c060ad603b9924ab9a4403f95dc88cc7
Vinay Jariya <vinayjariya@gmail.com>
2019-09-10 22:49:51 +0530

    Added Redux to the project

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner'; 
@@ -125,7 +126,7 @@ class ContactData extends Component{
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData,
         }
@@ -153,8 +154,8 @@ class ContactData extends Component{
         updatedOrderForm[inputIdentfier] = updatedFormElement;
 
         let formIsValid = true;
-        for(let inputIdentfier in updatedOrderForm){ 
-            formIsValid = updatedOrderForm[inputIdentfier].valid && formIsValid
+        for(var newinputIdentfier in updatedOrderForm){ 
+            formIsValid = updatedOrderForm[newinputIdentfier].valid && formIsValid
         }
 
         this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
@@ -198,4 +199,11 @@ class ContactData extends Component{
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice,
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
